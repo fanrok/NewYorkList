d171e2a5c976f0cac6466cc5012dada09a796b63
Ramark <fanrok@mail.ru>
2020-01-29 21:47:27 +0300

    add load data...and crash NetworkOnMainThreadException. i use corutines, WTF?

--- app/src/main/java/com/example/newyorklist/MainActivity.kt
+++ app/src/main/java/com/example/newyorklist/MainActivity.kt
@@ -5,9 +5,13 @@ package com.example.newyorklist
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.newyorklist.data.DatabaseHandler
+import com.example.newyorklist.data.LoadDataFromUrl
+import com.example.newyorklist.data.NewYorkJson
 import kotlinx.coroutines.*
 import com.example.newyorklist.data.Review
 
@@ -59,19 +63,39 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     var recyclerViewAdapter: RecyclerViewAdapter? = null
 //    var rowsArrayList: ArrayList<String?> = ArrayList()
     var rowsArrayList = mutableListOf<Review>()
-
+    val data = LoadDataFromUrl()
     var isLoading = false
+
+    val db = DatabaseHandler(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recyclerView = findViewById(R.id.recyclerView)
+        val search: SearchView = findViewById(R.id.search)
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
 
-        launch {
-            populateData(0)
-        }
-        initAdapter()
-        initScrollListener()
+                data.setQuery(query)
+                rowsArrayList.clear()
+                launch {
+                    val result = data.loadData()
+                    val reviews = addReviewsInDB(db, result.results, rowsArrayList)
+                    rowsArrayList = reviews
+//                    populateData(0)
+                    initAdapter()
+                    initScrollListener()
+                }
+
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+
     }
 
     suspend fun populateData(i: Int) = coroutineScope {
@@ -132,6 +156,50 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
             isLoading = false
         }, 2000)
+    }
+
+    fun addReviewsInDB(
+        db: DatabaseHandler,
+        reviews: List<NewYorkJson.Result>,
+        out: MutableList<Review>
+    ): MutableList<Review> {
+        for (review in reviews) {
+            if (!db.searchReviewByName(review.display_title)) {
+                if (review.multimedia != null) {
+                    out.add(
+                        Review(
+                            db.addReview(
+                                review.display_title,
+                                review.publication_date,
+                                review.summary_short,
+                                review.multimedia.src
+                            ),
+                            review.display_title,
+                            review.publication_date,
+                            review.summary_short,
+                            review.multimedia.src
+                        )
+                    )
+                } else {
+                    out.add(
+                        Review(
+                            db.addReview(
+                                review.display_title,
+                                review.publication_date,
+                                review.summary_short
+                            ),
+                            review.display_title,
+                            review.publication_date,
+                            review.summary_short,
+                            ""
+                        )
+                    )
+                }
+            } else {
+                out.add(db.getReviewByName(review.display_title))
+            }
+        }
+        return out
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
