60c38e8d8a2096cb66638e0c42e6f5033481ee3c
Dmitriy Larin <larin992@gmail.com>
2021-07-14 22:11:30 +0300

    -Добавил navigation component -переделал навигацию на компонент
    Signed-off-by: Dmitriy Larin <larin992@gmail.com>

--- app/src/main/java/com/example/newyorklist/ui/activities/entrypoint/EntryPointActivity.kt
+++ app/src/main/java/com/example/newyorklist/ui/activities/entrypoint/EntryPointActivity.kt
@@ -3,7 +3,6 @@ package com.example.newyorklist.ui.activities.entrypoint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.newyorklist.R
-import com.example.newyorklist.ui.fragments.newslist.NewsListFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,10 +11,5 @@ class EntryPointActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.entry_point)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, NewsListFragment.newInstance())
-                .commitNow()
-        }
     }
 }
