297fad91aa3f3833a7412d00325aee8370aa474d
Dmitriy Larin <larin992@gmail.com>
2021-07-14 22:52:59 +0300

    -сделал простенькую верстку для деталки -добавил в граф навигацию на деталку review
    Signed-off-by: Dmitriy Larin <larin992@gmail.com>

--- app/src/main/java/com/example/newyorklist/ui/fragments/reviewdetail/ReviewDetailFragment.kt
+++ app/src/main/java/com/example/newyorklist/ui/fragments/reviewdetail/ReviewDetailFragment.kt
@@ -1,6 +1,21 @@
 package com.example.newyorklist.ui.fragments.reviewdetail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.newyorklist.R
+
 /**
  * @author Dmitriy Larin
  */
-class ReviewDetailFragment
+class ReviewDetailFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return inflater.inflate(R.layout.review_detail, container, false)
+    }
+}
