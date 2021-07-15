fc1e6557f64543180b1e4fe9637b4671482a1c90
Dmitriy Larin <larin992@gmail.com>
2021-07-15 21:52:27 +0300

    -удалил ненужную папку из гита

--- app/src/main/java/com/example/newyorklist/domain/models/Review.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newyorklist.domain.models
-
-data class Review(
-    val id: Long = 0,
-    val name: String = "",
-    val date: String = "",
-    val text: String = "",
-    val img: String = ""
-)
--- app/src/main/java/com/example/newyorklist/ui/fragments/reviewslist/ReviewsListFragmentState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.newyorklist.ui.fragments.reviewslist
-
-import com.example.newyorklist.domain.models.Review
-
-sealed class ReviewsListFragmentState {
-    object Loading : ReviewsListFragmentState()
-    class Data(val list: List<Review>) : ReviewsListFragmentState()
-    object Empty : ReviewsListFragmentState()
-}
